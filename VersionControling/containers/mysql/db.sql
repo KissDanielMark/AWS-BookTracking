210cd6327ff90c76e611d1ecf7457060e8ff1844
DanMark <daniel.mark.kiss@gmail.com>
2023-10-29 22:27:07 +0100

    feat: :sparkles: docker here i come, established connection between two db-s

---
+++ VersionControling/containers/mysql/db.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE your_database;
+USE your_database;
+-- Create a new table
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    pwd VARCHAR(50)
+);
+
+-- Insert some data
+INSERT INTO users (username, pwd)
+VALUES 
+    ('Dan', 'pwd1'),
+    ('dan', 'pwd2');
+
+-- Query the data
+SELECT * FROM users;
--- VersionControling/containers/node_app/codebase_partner/app/config/config.js
+++ VersionControling/containers/node_app/codebase_partner/app/config/config.js
@@ -1,16 +1,18 @@
 // define default config, but allow overrides from ENV vars
 let config = {
   APP_DB_HOST: "3.82.161.206",
-  APP_DB_USER: "nodeapp",
-  APP_DB_PASSWORD: "coffee",
-  APP_DB_NAME: "COFFEE"
-}
+  APP_DB_USER: "root",
+  APP_DB_PASSWORD: "pwd",
+  APP_DB_NAME: "your_database",
+};
 
-Object.keys(config).forEach(key => {
-  if(process.env[key] === undefined){
-    console.log(`[NOTICE] Value for key '${key}' not found in ENV, using default value.  See app/config/config.js`)
+Object.keys(config).forEach((key) => {
+  if (process.env[key] === undefined) {
+    console.log(
+      `[NOTICE] Value for key '${key}' not found in ENV, using default value.  See app/config/config.js`
+    );
   } else {
-    config[key] = process.env[key]
+    config[key] = process.env[key];
   }
 });
 
